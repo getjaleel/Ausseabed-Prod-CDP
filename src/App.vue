ce5596c57a979a44a6d50ca05a2b70af019e12a9
Jaleel Ahmed Mohammad <16719397+getjaleel@users.noreply.github.com>
2022-06-08 04:26:08 +0530

    corrected to Ausseabed to AusSeabed

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
     
     <template v-slot:confirm-sign-up>
       <div style="padding: var(--amplify-space-large); text-align: center">
-      <h1>Thankyou for registring with Ausseabed.</h1>
+      <h1>Thankyou for registring with AusSeabed.</h1>
       <p>WATCH OUT FOR THE APPROVAL EMAIL IN YOUR INBOX AND PROCEED TO SIGN IN PAGE WITH SET CREDENTIALS.CLICK ON RETURN TO SIGN IN BUTTON BELOW</p>
       <p class="amplify-text" style="color: var(--amplify-colors-red-80)">
       <button @click="signInButton()">Return to Sign In</button>
